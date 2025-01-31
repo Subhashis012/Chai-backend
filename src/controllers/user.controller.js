3581b3863d290edb3e8176ba2898dfd58bdc6c54
Subhashis <subhashisdhara123@gmail.com>
2025-01-31 15:14:53 +0530

    fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,7 +24,7 @@ const registerUser = asyncHandler(async (req, res) => {
        throw new ApiError(400, "All fields are required");
     }
 
-    const existedUser = User.findOne({
+    const existedUser = user.findOne({
         $or: [{ email }, { username }],
     })
 
@@ -35,16 +35,20 @@ const registerUser = asyncHandler(async (req, res) => {
     const avatarLocalPath = req.files?.avatar[0]?.path;
     const coverImageLocalPath =  req.files?.coverImage[0]?.path;
 
+
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar is required");
     }
 
+
     const avatar =  await uploadOnCloudinary(avatarLocalPath)
     const coverImage =  await uploadOnCloudinary(coverImageLocalPath)
+
 
     if (!avatar) {
         throw new ApiError(400, "Error uploading avatar");
     }
+
 
     const user = await User.create({
         fullName,
@@ -55,13 +59,16 @@ const registerUser = asyncHandler(async (req, res) => {
         password
     })
 
+
     const createdUser =  await User.findById(user._id).select(
       "-password -refreshToken"
     )
 
+
     if (!createdUser) {
         throw new ApiError(500, "Something went wrong while registering the user");
     }
+
 
     return res.status(201).json(new ApiResponse(
       200, createdUser, "User registered Successfully")
